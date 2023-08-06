db737fa1a4874f3818b1c9a58ac860acb52cc149
Rodion Pushkarskiy <pushkarskiyrodion@gmail.com>
2023-08-06 18:29:24 +0300

    phone catalog was done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,137 @@
+import { useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from 'app/hooks/redux';
+import {
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import './App.scss';
 
-const App = () => (
-  <div className="App">
-    <h1>React Phone Catalog</h1>
-  </div>
-);
+import { actions as productsActions } from 'features/productsReducer';
+import { ProductDetails } from 'components/ProductDetails';
+import { NotFound } from 'components/ErrorPages/NotFound';
+import { Header } from 'components/Header';
+import { Footer } from 'components/Footer';
+import { Loader } from 'components/Loader';
+
+import { FavouritePage } from 'pages/FavouritePage';
+import { PageNotFound } from 'pages/PageNotFound';
+import { ProductPage } from 'pages/ProductPage';
+import { CartPage } from 'pages/CartPage';
+import { HomePage } from 'pages/HomePage';
+
+import { getProducts } from 'api/product';
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const products = useAppSelector(state => state.products);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+
+        const phones = await getProducts();
+
+        dispatch(productsActions.set({
+          phones,
+          accessories: [],
+          tablets: [],
+        }));
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (isError) {
+    return <NotFound />;
+  }
+
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={(
+            <>
+              <Header />
+              <Outlet />
+              <Footer />
+            </>
+          )}
+        >
+          <Route index element={<HomePage products={products} />} />
+
+          <Route path="phones">
+            <Route
+              index
+              element={(
+                <ProductPage
+                  productsCategory={products.phones}
+                  title="Mobile phones"
+                />
+              )}
+            />
+            <Route
+              path=":productId"
+              element={<ProductDetails productsCategory={products.phones} />}
+            />
+          </Route>
+
+          <Route path="tablets">
+            <Route
+              index
+              element={(
+                <ProductPage
+                  productsCategory={products.tablets}
+                  title="Tablets"
+                />
+              )}
+            />
+            <Route
+              path=":productId"
+              element={<ProductDetails productsCategory={products.tablets} />}
+            />
+          </Route>
+
+          <Route path="accessories">
+            <Route
+              index
+              element={(
+                <ProductPage
+                  productsCategory={products.accessories}
+                  title="Accessories"
+                />
+              )}
+            />
+            <Route
+              path=":productId"
+              element={(
+                <ProductDetails productsCategory={products.accessories} />
+              )}
+            />
+          </Route>
+
+          <Route path="favourite" element={<FavouritePage />} />
+          <Route path="cart" element={<CartPage />} />
+          <Route path="home" element={<Navigate to="/" replace />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from 'app/store';
 
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
   document.getElementById('root'),
 );
